5ee228cde1cc57d7464fcbaaa7b9d2d9b8af8e68
Anna Faytelson <af654@scarletmail.rutgers.edu>
2018-11-18 21:01:13 -0500

    Update prm.py

--- part1code/prm.py
+++ part1code/prm.py
@@ -526,7 +526,7 @@ def reposition_robot(vertex):
 
 def main():
     startTime = datetime.now()
-    road_map = RoadMap(FixedKPRM())
+    road_map = RoadMap(ConnectedComponentPRM())
     fda = APath(road_map)
     graph = road_map.graph
 
